b11fe17d4f8e9187028e157168dcdd5278bf1bd2
Demian Yovanovich <subwaydemiany@gmail.com>
2022-09-17 13:51:43 -0300

    validaciones agregadas

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
